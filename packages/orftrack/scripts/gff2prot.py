7ff1038f9c60611233ffde91f273c0a8d9b55fac
nchenche <nicolas.chevrollier@laposte.net>
2022-11-30 19:43:57 +0100

    restructure gff2prot library dependance

--- packages/orftrack/scripts/gff2prot.py
+++ packages/orftrack/scripts/gff2prot.py
@@ -1,21 +1,17 @@
 import argparse
-import logging
-import os
 from pathlib import Path
 import re
 import sys
-from threading import Thread
 from typing import List
 import time
 
-import psutil
 
-from packages.orftrack.lib import gff_parser, fasta_parser, inspect
+from packages.orftrack.lib import gff_parser, fasta_parser
+from packages.orftrack.lib.utils import CDSQueue, get_chunks, index_genomes, validate_chromosomes
 
 import multiprocessing
 
 
-logger = logging.getLogger()
 
 def get_args():
     """
@@ -87,170 +83,6 @@ def get_args():
     args = parser.parse_args()
 
     return args
-
-
-class IndexFasta(Thread):
-    def __init__(self, filename):
-        super().__init__()
-        self.filename = filename
-        self.result = None
-
-    def run(self):
-        print(f"{self.name} - Indexing fasta file...")
-        start_time = time.time()
-        self.result = fasta_parser.parse(fasta_filename=self.filename)
-        print(f"{self.name} - Fasta file parsed in {round(time.time()-start_time, 2)} seconds")
-
-
-class IndexGFF(Thread):
-    def __init__(self, filename):
-        super().__init__()
-        self.filename = filename
-        self.result = None
-
-    def run(self):
-        print(f"{self.name} - Indexing GFF file...")
-        start_time = time.time()
-        self.result = set_gff_descr(gff_fname=self.filename)
-        print(f"{self.name} - GFF file parsed in {round(time.time()-start_time, 2)} seconds")
-
-
-
-class CDSQueue:
-    """
-    Object used to process CDS, i.e. make them ready to be written, nearly "on-the-fly".
-
-
-    Workflow attributes
-    -------------------
-    cds_list: List[gff_parser.GffElement] = []  # stores parent-related CDS unready
-    stored_parent: str = ""  # stored last read parent
-    cds_completed: List[gff_parser.GffElement] = []  # stores parent-related CDS ready to be merged
-    
-
-    Workflow explanation
-    --------------------
-
-    Every time a CDS is read, it is added to the CDSQueue instance.
-    When a CDS is added, we first check if the precedently stored parent 
-    is the same as the parent of the currently read/added CDS:
-        - if both parents are the same, we add the current CDS to cds_list and clear cds_completed content
-        - if both parents are different:
-            1. we copy cds_list elements into the cds_completed list
-            2. we update the stored parent value
-            3. we clear the cds_list content
-            4. we add the current CDS to cds_list
-
-    To know when CDS is ready to be written (i.e fully merged in case of multi-exon proteins),
-    we just have to check if queue.cds_completed is empty or not. If not empty, CDS can we written.
-
-    
-    Workflow illustration
-    --------------------
-
-    actual CDS      stored.parent   update()?   stored.parent   cds_completed status            cds_list status
-    ----------      -------------   ---------   -------------   --------------------            ---------------
-    cds1-parentA    None            YES         parentA         []                              [cds1-parentA]
-    cds1-parentB    parentA         YES         parentB         [cds1-parentA]                  [cds1-parentB]
-    cds2-parentB    parentB         NO          parentB         []                              [cds1-parentB, cds2-parentB]
-    cds1-parentC    parentB         YES         parentC         [cds1-parentB, cds2-parentB]    [cds1-parentC]
-    ...
-
-
-    Workflow usage pseudo-code
-    --------------------------
-    queue = CDSQueue()
-
-    for line in gff_file:
-        element_type = line.split('\t')[2]
-        gff_element = gff_parser.GffElement(gff_line=line, fasta_chr=fasta_chr)
-
-        # we add to the queue every CDS element
-        if gff_element.type == "CDS":
-            queue.add(cds=gff_element)
-
-        # we check if CDS are ready to be written (i.e. merged in case of multi-exonic proteins)
-        if queue.cds_completed:
-            # print(", ".join([x.name for x in queue.cds_completed]))
-            write(queue.get_fasta())
-
-    """
-    def __init__(self) -> None:
-        self.cds_list: List[gff_parser.GffElement] = []  # stores parent-related CDS unready
-        self.stored_parent: str = ""  # stored last read parent
-        self.cds_completed: List[gff_parser.GffElement] = []  # stores parent-related CDS ready to be merged
-
-    def add(self, cds: gff_parser.GffElement=None):
-        # when CDS parent has not been met yet, we update the state of CDSQueue instance
-        if cds.parent != self.stored_parent:
-            self.update(cds)
-        else:
-            self.cds_completed.clear()
-
-        if cds.strand == "-":
-            if not self.cds_list:
-                self.cds_list.append(cds)
-            else:
-                if cds.start < self.cds_list[0].start:
-                    self.cds_list.append(cds)
-                else:
-                    self.cds_list.insert(0, cds)
-        else:
-            self.cds_list.append(cds)
-
-
-    def update(self, cds: gff_parser.GffElement=None):
-        self.stored_parent = cds.parent
-
-        self.cds_completed = [x for x in self.cds_list]
-        self.cds_list.clear()
-    
-    def get_fasta(self, _type="nucleic"):
-        try:
-            header = '>' + self.cds_completed[0].name + '\n'
-        except:
-            print(f"Exception, list error: {self.cds_completed}")
-        if _type == "nucleic":
-            return header + ''.join([cds.sequence() for cds in self.cds_completed]) + '\n'
-        else:
-            return header + ''.join([cds.translate() for cds in self.cds_completed]) + '\n'   
-
-    def elongate(self, value):
-        # elongation of CDS in borders only
-        if self.cds_completed[0].strand == "-":
-            cds_first = self.cds_completed[-1]
-            cds_last = self.cds_completed[0]
-        else:
-            cds_first = self.cds_completed[0]
-            cds_last = self.cds_completed[-1]
-
-        cds_first.start = cds_first.start - value
-        cds_last.end = cds_last.end + value
-
-
-def memory_usage():
-    # return the memory usage in MB
-    process = psutil.Process(os.getpid())
-    mem = process.memory_info().rss / float(2 ** 20)
-
-    return mem
-
-
-def set_gff_descr(gff_fname):
-    gff_indexes = {}
-        
-    with open(gff_fname, 'rb') as gff_file:
-        line = gff_file.readline().decode(encoding='utf-8')
-        while line:
-            if not line.startswith('#'):
-                name = line.split('\t')[0]
-                pos_chr = gff_file.tell()-len(line)
-                if name not in gff_indexes:
-                    gff_indexes[name] = pos_chr
-                
-            line = gff_file.readline().decode(encoding='utf-8')
-
-    return gff_indexes
 
 
 def process_chromosome(gff_filename: str=None, fasta_chr: fasta_parser.Fasta=None, chr_id: str="", features: list=[], basename_out: str="", out_formats: list=[".pfasta", ".nfasta"], elongation: int=None, gff_indexes: dict={}) -> dict:
@@ -377,40 +209,6 @@ def process_chromosome(gff_filename: str=None, fasta_chr: fasta_parser.Fasta=Non
     print(f"{process_name} -  Chromosome {chr_id} successfully processed in {round(time.time()-start_time, 2)} seconds")
                                 
 
-
-def get_chunks(l: list, chunks_size: int=10):     
-    for i in range(0, len(l), chunks_size):
-        yield l[i:i+chunks_size]
-
-
-def validate_chromosomes(to_include: list=[], to_exclude: list=[], in_gff: list=[], in_fasta: list=[]) -> list:
-    # get chromosomes present both in the GFF file and the fasta file
-    chrs_common = inspect.check_chrids(chrs_gff=sorted(in_gff), chrs_fasta=sorted(in_fasta))
-
-    # get chromosomes to be treated
-    if not to_include:
-        chr_ids = sorted([x for x in chrs_common if x not in to_exclude])
-    else:
-        chr_ids = sorted([x for x in to_include if x not in to_exclude and x in chrs_common])
-
-    for chromosome in to_include:
-        if chromosome not in chr_ids:
-            logger.warning(f"Warning: chromosome {chromosome} not found in either the fasta or GFF file. It will be skipped...")
-
-    return chr_ids
-
-
-def index_genomes(fasta_file: str="", gff_file: str=""):
-    indexing_threads = [IndexFasta(filename=fasta_file), IndexGFF(filename=gff_file)]
-    for t in indexing_threads:
-        t.start()
-
-    for t in indexing_threads:
-        t.join()
-
-    return [x.result for x in indexing_threads]
-
-
 def main():
     args = get_args()
     genomic_fna = args.fna
@@ -443,7 +241,10 @@ def main():
     # chromosomes must be consistent between the fasta file and gff file
     # if a same chromosome is given in both chr_includes and chr_excludes, this chromosome will be included.
     valid_chromosomes = validate_chromosomes(to_include=chromosomes_to_process, to_exclude=chromosomes_to_exclude, in_gff=gff_indexes.keys(), in_fasta=fasta_hash.keys())
-    print("Chromosomes that will be processed: {}".format(", ".join(valid_chromosomes)))
+    print("Chromosomes that will be processed:")
+    for chr in valid_chromosomes:
+        print(f" - {chr}")
+    print()
 
     chunks = get_chunks(l=valid_chromosomes, chunks_size=cpus)
 
