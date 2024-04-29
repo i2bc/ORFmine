5e2c7696e8c91640616816ae9f51d8947b15d3ce
Fadwa EL KHADDAR <94709894+Fadwa7@users.noreply.github.com>
2024-04-29 10:17:43 +0200

    Delete venv-3.9 directory

--- venv-3.9/lib/python3.9/site-packages/cutadapt/__main__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from cutadapt.cli import main_cli
-
-if __name__ == "__main__":
-    sys.exit(main_cli())
--- venv-3.9/lib/python3.9/site-packages/yte/exceptions.py
+++
@@ -1,6 +0,0 @@
-class YteError(Exception):
-    def __init__(self, msg, context):
-        section = (
-            "in section /" + "/".join(context.template) if context else "at top level"
-        )
-        super().__init__(f"Error processing template {section}: {msg}")
