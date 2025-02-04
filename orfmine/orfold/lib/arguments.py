6690b831c03010704657363a859f6d25c45b21bc
Fadwa7 <fadwa.elkhaddar@gmail.com>
2025-02-04 11:51:05 +0100

    Modification of Dockerfile to avoir permission error in Singularity usage and modification of argument in orfold for IUPRED and TANGO

--- orfmine/orfold/lib/arguments.py
+++ orfmine/orfold/lib/arguments.py
@@ -11,13 +11,13 @@ def get_args():
 
     parser.add_argument("--faa", "-F", type=str, required=True, help="FASTA file containing the amino acid sequences to treat")
     parser.add_argument("--gff", "-G", type=str, required=False, help="GFF annotation file")
-    parser.add_argument("--options", "-P", type=str, required=True, help="Which properties are to be calculated. H for HCA (Default), I for IUPred, T for Tango")
+    parser.add_argument("--options", "-P", type=str, required=True, help="Which properties are to be calculated. H for HCA (Default), I for IUPred, T for Tango (e.g. `-options HI` or `-options -HT` or `-options HIT` ...)")
     parser.add_argument("--out", "-O", type=str, required=False, default='.', help="Output directory ('.' by default).")
     parser.add_argument("--barcodes", required=False, action='store_true', default=False, help=argparse.SUPPRESS)
     parser.add_argument("--keep", "-K", required=False, action='store_true', default=False, help="Option for keeping the Tango output files")
     parser.add_argument("--sample", "-N", type=int, required=False, default=-1, help="Size of the sample to use for the fasta sequences. Defaults to -1 (i.e. all sequences).")
-    parser.add_argument("--path_tango", type=str, required=True, help="Path to the Tango executable")
-    parser.add_argument("--path_iupred", type=str, required=True, help="Path to the IUPred directory")
+    parser.add_argument("--path_tango", type=str, required=False, help="Path to the Tango executable")
+    parser.add_argument("--path_iupred", type=str, required=False, help="Path to the IUPred directory")
 
 
 
